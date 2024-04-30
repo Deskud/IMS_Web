d6acd071972df64cc8801f756463b5551ef63f62
Deskud <darrylgambe@gmail.com>
2024-04-30 10:30:40 +0800

    added Select size, quantity, adn product ID - Edit function not yet finished pag pinipindot di nag re-redirect sa edit page-ayusin ko pa

--- Parts/AddProducts.php
+++ Parts/AddProducts.php
@@ -4,29 +4,35 @@
 
     if(isset($_POST['Add'])){
 
-        $product_name = $_POST['product_name'];
-        $product_price = $_POST['product_price'];
+
+        $product_name = $_POST['name'];
+        $product_price = $_POST['price'];
+        $product_size = $_POST['size'];
+        $product_quantity = $_POST['quantity'];
+
+
 
         if(empty($product_name)|| empty($product_price)){
             $message[] = 'Please fill out the following';
 
         }
         else{
-            $insert = "INSERT INTO uniform_products(Name, Price) VALUES('$product_name', '$product_price')";
+            $insert = "INSERT INTO uniform_products(product_name, price, product_size, product_quantity) VALUES('$product_name', '$product_price', '$product_size', '$product_quantity')"; 
 
             $adding = mysqli_query($dbADD, $insert);
 
-            if($adding){
+            if($adding){// If the addition of the product is successfull say this
                 $message[] = 'Product added successfully';
 
             }
-            else{
+            else{ //Check errors
                 $message[] = 'Bruh';
 
             }
         }
     };
 
+    //Shows error message -> look at line 12
     if(isset($message)){
         foreach($message as $message){
             echo'<span class="message">'.$message.'</span>';
@@ -34,13 +40,13 @@
         }
     } 
 
-    if(isset($_GET['delete'])){
+    //Deletes the data from table and the database. Basis is id/product_id
+    if(isset($_GET['delete'])){ 
         $id = $_GET['delete'];
-        mysqli_query($dbADD, "DELETE FROM uniform_products WHERE ID = $id");
+        mysqli_query($dbADD, "DELETE FROM uniform_products WHERE product_id = $id");
         header('location:Products.php');
 
     }
-
 ?>
 
 
@@ -49,8 +55,20 @@
     <div class="AddProd">
         <form action="" method="post" enctype='multipart/form-data'>
             <h1>Add Products</h3>
-            <input type="text" placeholder="Enter Product" name="product_name" class="boxprod">
-            <input type="number" placeholder="Enter Price" name="product_price" class="boxprice">
+            <input type="text" placeholder="Enter Product" name="name" class="boxprod">
+            <input type="number" placeholder="Enter Price" name="price" class="boxprice">
+            <input type="number" placeholder="Enter Quantity" name="quantity" class="boxquantity">
+
+                <select class="selectsize" name="size">
+                <option selected>Select Uniform Size</option>
+                <option value="Small">Small</option>
+                <option value="Medium">Medium</option>
+                <option value="Large">Large</option>
+                <option value="Extra Large">Extra Large</option>
+                </select>
+
+
+
             <input type="submit" name="Add" class="Addbutton">
         </form>
        
@@ -70,10 +88,19 @@ $select = mysqli_query($dbADD, "SELECT * FROM uniform_products");
     <table class="display_added_prod">
         <tr>
             <td>
+                Product Id
+            </td>
+            <td>
                 Product Name
             </td>
             <td>
                 Product Price
+            </td>
+            <td>
+                Product Size
+            </td>
+            <td>
+                Product Quantity
             </td>
             <td colspan="2">
                 Action
@@ -86,14 +113,23 @@ $select = mysqli_query($dbADD, "SELECT * FROM uniform_products");
     ?>     
             <tr>
                 <td>
-                    <?php echo $row['Name'] ?>
+                    <?php echo $row['product_id'] ?>
                 </td>
                 <td>
-                    P<?php echo $row['Price'] ?>
+                    <?php echo $row['product_name'] ?>
                 </td>
                 <td>
-                    <a href="AdminEdit.php?edit=<?php echo $row['ID'];?>"class="btnEdit"><i class="fa-solid fa-pen-to-square"></i>Edit</i></a>
-                    <a href="Products.php?delete=<?php echo $row['ID'];?>"class="btnEdit"><i class="fa-solid fa-trash"></i>Delete</i></a>
+                    P<?php echo $row['price'] ?>
+                </td>
+                <td>
+                    <?php echo $row['product_size'] ?>
+                </td>
+                <td>
+                    <?php echo $row['product_quantity'] ?>
+                </td>
+                <td>
+                    <a href="AdminEdit.php?edit=<?php echo $row['product_id'];?>"class="btnEdit"><i class="fa-solid fa-pen-to-square"></i>Edit</i></a>
+                    <a href="Products.php?delete=<?php echo $row['product_id'];?>"class="btnEdit"><i class="fa-solid fa-trash"></i>Delete</i></a>
                 </td>
             
             </tr>
--- Parts/AdminEdit.php
+++ Parts/AdminEdit.php
@@ -1,4 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/IMSstyle.css">
+
+    <title>Update Product</title>
+</head>
+<body>
 <?php
+                        
+    if(isset($message)){
+            foreach($message as $message){
+            echo'<span class="message">'.$message.'</span>';
+                    
+        }
+    } 
+                        
+ ?>             
+            <div class="Productcontainer">
+                <h1>Update Product</h1>
+                <div class="AddProd">
+                    <form action="" method="post" enctype='multipart/form-data'>
+                    <h1>Add Products</h3>
+                    <input type="text" placeholder="Enter Product" name="update_name" class="boxprod">
+                    <input type="number" placeholder="Enter Price" name="update_price" class="boxprice">
+                    <input type="number" placeholder="Enter Quantity" name="update_quantity" class="boxquantity">
 
-
-?>
+                    <select class="selectsize" name="update_size">
+                    <option selected>Select Uniform Size</option>
+                    <option value="Small">Small</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Large">Large</option>
+                    <option value="Extra Large">Extra Large</option>
+                    </select>
+                    <input type="submit" name="Update_Add" value="update_product"  class="Addbutton">
+                    <a href="Products.php" class="upbutton">Go Back</a>
+                    </form>
+            </div>
+        </div>
+</body>
+</html>
